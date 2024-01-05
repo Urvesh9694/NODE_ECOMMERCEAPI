da9fd6b131ea267dc682dbb253cf1ccc9bddfab4
Urvesh9694 <190770107590@socet.edu.in>
2024-01-05 16:30:06 +0530

    5-jan add

---
+++ routes/verifyToken.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+
+const verifytoken = (req,res,next) =>{
+    const authHeader = req.header.token
+    if(authHeader){
+        const token = authHeader.split("")[1];
+        jwt.verify(token, process.env.JWT_SEC,(err,user) =>{
+            if(err) res.status(403).json("token is not valid!");
+            req.user = user;
+            next();
+        })
+    }else{
+        return res.status(401).json("you are not authenticated!");
+    }
+};
+
+const verifyTokenAndAuthorization = (req,res,next) =>{
+    verifytoken(req,res,() =>{
+        if(req.user.id === req.parms.id || req.user.isAdmin){
+            next();
+        }else{
+            res.status(403).json("you are not alowed to do that!");
+        }
+    })
+}
+
+module.exports = {verifytoken , verifyTokenAndAuthorization}
